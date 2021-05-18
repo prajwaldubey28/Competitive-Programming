0e483f1318f2522489131a48c20048d99cccd775
prajwaldubey28 <61945047+prajwaldubey28@users.noreply.github.com>
2021-05-18 22:01:37 +0530

    Update Rough Paper.py

--- Codechef/Rough Paper.py
+++ Codechef/Rough Paper.py
@@ -1,12 +1,13 @@
 # Q = https://www.codechef.com/SHAK2021/problems/KRAFT
 
 try:
+    import math
     n = int(input())
     while n>0:
         l,b = map(int,input().split())
-        x = int(l/2)
-        y = int(b/x)
-        print(2*y)
+        ans = math.gcd(l,b)**2
+        final = (l*b)//ans
+        print(final)
         
         n = n-1
 except:
